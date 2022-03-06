65bf90493909549a17f7734bcc39b10f03400f74
Mohammad Ibnu <siibnu99@gmail.om>
2022-03-06 12:39:15 +0700

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FC6011",
+        primaryFont: "#4a4b4d",
+        secondary: "#7c7d7e",
+        placeholder: "#b6b7b7",
+      },
+    },
+  },
+  plugins: [],
+};
