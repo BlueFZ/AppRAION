a8648c501e835758e2d76539693de77a8c975477
BlueFZ <muhammadsyafi@student.ub.ac.id>
2022-03-06 22:22:36 +0700

    fix

--- App.js
+++ App.js
@@ -1,12 +1,3 @@
-//<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import react from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-//=======
 import { StatusBar } from "expo-status-bar";
 import react from "react";
 import { StyleSheet, Text, View } from "react-native";
@@ -15,7 +6,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import MainNavigation from "./navigation/MainNavigation";
-//>>>>>>> 6db179c571ab9d323a6f9bfa65ff081a7d4174e6
 
 const Stack = createNativeStackNavigator();
 
